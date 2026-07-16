723d97691c0f272c74e7844dac5b00fff465f04e
Francisco Javier Català Balta <fcatala-@student.42barcelona.com>
2026-07-16 09:07:54 +0200

    feat: add ft_garden_intro.py with shebang line (but not made it executable yet)

---
+++ Module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro():
+    """
+    This function displays information about a
+     plant in the garden..
+    """
+    print("=== Welcome to My Garden === ")
+    plant = "Rose"
+    height = 25
+    age = 30
+    print(f"{plant.capitalize()} in the garden.")
+    print(f"Height: {height} cm")
+    print(f"Age: {age} days")
+    print("=== End of Program === ")
+
+def main():
+    ft_garden_intro()
+
+if  __name__ == "__main__":
+    main()
